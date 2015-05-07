644b3e389b7e2dfc8893a28473462db265a2bb21
Taweerat Chaiman <taweesoft@gmail.com>
2015-05-07 23:54:06 +0700

    Done alert dialog to confirm accept from friend request

--- app/src/main/java/com/example/taweesoft/joinpa/FriendRequest/FriendRequestCustomAdapter.java
+++ app/src/main/java/com/example/taweesoft/joinpa/FriendRequest/FriendRequestCustomAdapter.java
@@ -1,6 +1,8 @@
 package com.example.taweesoft.joinpa.FriendRequest;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,24 +54,50 @@ public class FriendRequestCustomAdapter extends ArrayAdapter<Friend>{
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.friend_waiting_list_view,null);
         TextView txt_username = (TextView)convertView.findViewById(R.id.txt_username);
         Button btn_accept = (Button)convertView.findViewById(R.id.btn_accept);
-        btn_accept.setOnClickListener(new AcceptFriend(friend));
+        btn_accept.setOnClickListener(new ShowConfirmDialog(friend));
         txt_username.setText(friendName);
         return convertView;
     }
 
     /**
-     * Accept friend request.
+     * Show confirm dialog
      */
-    class AcceptFriend implements View.OnClickListener{
+    class ShowConfirmDialog implements View.OnClickListener{
         private Friend friend;
-        public AcceptFriend(Friend friend){
+        public ShowConfirmDialog(Friend friend){
             this.friend = friend;
         }
         @Override
         public void onClick(View v) {
+            showConfirmDialog(friend);
+        }
+    }
+    /**
+     * Accept friend request.
+     */
+    class AcceptFriend implements DialogInterface.OnClickListener{
+        private Friend friend;
+        public AcceptFriend(Friend friend){
+            this.friend = friend;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
             Resources.owner.acceptFriend(friend);
             observable.setChanged();
             observable.notifyObservers();
         }
+    }
+
+    /**
+     * Show confirm dialog
+     * @param friend
+     */
+    public void showConfirmDialog(Friend friend){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(getContext());
+        dialog.setMessage(String.format("Accept friend request from %s",friend.getUsername()));
+        dialog.setPositiveButton("Yes",new AcceptFriend(friend));
+        dialog.setNegativeButton("No",null);
+        dialog.show();
     }
 }
